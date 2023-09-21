28ebbcbbff0826cdfa12ba1c8500efcd67fd83ed
paulohenrique-gh <ph.1928@hotmail.com>
2023-09-21 11:56:51 -0300

    add menu functions

--- gerenciador_tarefas.rb
+++ gerenciador_tarefas.rb
@@ -8,19 +8,33 @@ puts "Informe o caminho do arquivo com a extensão. Ex.: tarefas.txt"
 print "> "
 file_name = gets.chomp
 
-$tasks = extract_from_file(file_name)
+$tasks = []
+
+if File.exist?(file_name)
+    $tasks = extract_from_file(file_name)
+end
+
+$new_tasks = []
 
 puts
 
-puts "Digite uma das opções:"
-puts "1. Criar nova tarefa"
-puts "2. Listar todas as tarefas"
-puts "3. Marcar tarefa como concluída"
-puts "4. Remover tarefa da lista"
-puts "5. Salvar lista"
-puts "6. Sair"
-print "> "
+loop do
+    puts "Digite uma das opções:"
+    puts "1. Criar nova tarefa"
+    puts "2. Listar todas as tarefas"
+    puts "3. Marcar tarefa como concluída"
+    puts "4. Remover tarefa da lista"
+    puts "5. Salvar"
+    puts "6. Sair"
+    print "> "
+    option = gets.chomp
 
-# loop do
-    
-# end
+    case option
+    when "1"
+        create_task()
+    when "2"
+        list_tasks()
+    when "6"
+        exit(0)
+    end
+end
--- helpers.rb
+++ helpers.rb
@@ -5,7 +5,7 @@ def extract_from_file(file_name)
 
     File.readlines(file_name, chomp: true).each do |line|
         split_task = line.split(' - ')
-        number = split_task[0]
+        number = split_task[0].to_i
         description = split_task[1]
         status = split_task[2]
         task = { number: number, description: description, status: status }
